6d6977ba67d86ea35dab886c9cf7fe7e23a6ea93
Axenior <137138883+Holis09@users.noreply.github.com>
2024-12-06 18:15:48 +0700

    update

--- express_app/skillconnect/app.js
+++ express_app/skillconnect/app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/app_server/index');
+var usersRouter = require('./routes/app_server/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server' ,'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
