fd1e3f9e75f604a88752cdf5126cb40493a20d7a
Holis09 <137138883+Holis09@users.noreply.github.com>
2024-12-11 00:29:16 +0700

    UPDATE

--- express_app/skillconnect/app.js
+++ express_app/skillconnect/app.js
@@ -4,19 +4,19 @@ const connectDB = require('./app_server/database/db');
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Koneksi Database
+
 connectDB();
 
-// Test route
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
 
-// Error handling
+
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Route not found' });
 });
@@ -26,10 +26,10 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// Handle unhandled promise rejections
+
 process.on('unhandledRejection', (err) => {
   console.log('Unhandled Rejection:', err);
-  // Close server & exit process
+
   server.close(() => process.exit(1));
 });
 
